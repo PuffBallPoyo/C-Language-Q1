5e01cf910679b526dd0c58490d7dad47df76865f
PuffBallPoyo <46196874+PuffBallPoyo@users.noreply.github.com>
2019-01-08 19:50:21 +0100

    double when returning void lul

--- Less of a mess/exDamageFunc.c
+++ Less of a mess/exDamageFunc.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-    double showDamage(double damage, double modifier);
+    void showDamage(double damage, double modifier);
     //Affiche le produit des dégats et de son modificateur sans renvoie à la fonction main
     double showDamageToMain(double damage, double modifier);
     //Calcule le produit des dégats et de son modificateur et les renvoie à la fonction main
@@ -11,7 +11,7 @@ void main(void) {
     showDamage(100.0,0.5);
     printf("Damage with modifier : %.1f\n",showDamageToMain(1000.0,0.5));
 }
-double showDamage(double damage, double modifier){
+void showDamage(double damage, double modifier){
     printf("Damage with modifier : %.1f\n",damage*modifier);
 }
 double showDamageToMain(double damage, double modifier){
